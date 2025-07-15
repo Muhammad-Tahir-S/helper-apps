586af598b3f7e34222ddf60455b0d92410fdf302
adeke910 <adekemibamiteko@gmail.com>
2025-07-15 21:24:54 +0100

    input component + typography

--- frontend/src/shared/components/ui/input.tsx
+++ frontend/src/shared/components/ui/input.tsx
@@ -1,16 +1,16 @@
+import type { ReactNode } from "react";
 import * as React from "react";
 
 import { cn } from "@/shared/lib/utils";
 
+import CheckCircle from "../../assets/icons/check-circle.svg";
+import RemoveCircle from "../../assets/icons/remove-circle.svg";
 import Typography from "../typography";
 
 interface InputProps extends React.ComponentProps<"input"> {
-  withRightIcon?: boolean;
-  withLeftIcon?: boolean;
-  state?: "default" | "typing" | "disabled" | "error";
+  rightIcon?: ReactNode;
+  leftIcon?: ReactNode;
   label?: string;
-  // placeholder?: string;
-  buttonText?: boolean;
   helperText?: string;
 }
 
@@ -18,31 +18,61 @@ function Input({
   className,
   type = "text",
   label,
-  // withRightIcon = false,
-  // withLeftIcon = false,
-  // state = "default",
-  // placeholder,
-  // buttonText,
-  // helperText,
+  rightIcon,
+  leftIcon,
+  helperText,
   ...props
 }: InputProps) {
+  const isInvalid = props["aria-invalid"] === true;
+
   return (
-    <div className="flex flex-col gap-2">
-      <Typography variant="text-sm" fontWeight="medium">
-        {label}
-      </Typography>
-      <div className="flex flex-col gap-1.5">
-        <input
-          type={type}
-          data-slot="input"
-          className={cn(
-            "file:text-foreground placeholder:text-gray-500 selection:bg-primary selection:text-primary-foreground dark:bg-gray-850 border-input flex h-13 w-full min-w-0 rounded-md border bg-gray-100 px-3 py-1 text-gray-800 shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-            "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-            "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-            className,
-          )}
-          {...props}
-        />
+    <div className="flex flex-col gap-1.5">
+      <div className="flex flex-col gap-2">
+        <Typography variant="text-sm/medium" className="dark:text-gray-100">
+          {label}
+        </Typography>
+        <div className="flex items-center relative">
+          <input
+            type={type}
+            data-slot="input"
+            className={cn(
+              "peer",
+              "file:text-foreground placeholder:text-gray-400 selection:bg-primary selection:text-primary-foreground dark:bg-gray-850 border-input flex h-13 w-full min-w-0 rounded-md border bg-gray-100 px-3 py-1 text-base-dark dark:text-primary-50 shadow-xs transition-[color,box-shadow] outline-none font-medium file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-400  md:text-sm ",
+
+              " focus:border-primary-400 focus:bg-transparent focus:dark:border-primary-hover focus:border-[2px]",
+
+              "aria-invalid:border-error-main aria-invalid:text-error-main dark:aria-invalid:border-error-main dark:aria-invalid:text-error-main ",
+              "[&:not(:focus):not(:placeholder-shown)]:bg-primary-25 dark:[&:not(:focus):not(:placeholder-shown)]:bg-transparent",
+              className,
+              leftIcon ? "pr-12" : "",
+              rightIcon ? "pl-12" : "",
+            )}
+            {...props}
+          />
+          <div
+            className={cn(
+              "absolute items-center left-4 text-gray-400  h-[24px] w-[24px] peer-focus:text-primary-main peer-focus:dark:text-primary-hover peer-disabled:dark:text-gray-400 ",
+              isInvalid
+                ? "peer-[&:not(:focus):not(:placeholder-shown)]:dark:text-gray-500"
+                : "peer-[&:not(:focus):not(:placeholder-shown):not(:disabled)]:dark:text-primary-main peer-[&:not(:focus):not(:placeholder-shown)]:text-gray-500",
+            )}
+          >
+            {rightIcon}
+          </div>
+          <div className="absolute items-center right-4 text-gray-400 h-[20px] w-[20px]">
+            {leftIcon}
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-0.5">
+        {isInvalid ? (
+          <RemoveCircle className="fill-error-main" />
+        ) : (
+          <CheckCircle className="fill-gray-400" />
+        )}
+        <Typography variant="text-sm/medium" className="text-gray-500">
+          {helperText}
+        </Typography>
       </div>
     </div>
   );
